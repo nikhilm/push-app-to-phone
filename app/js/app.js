9761623ee48c192f0730165daf4fcac4fafeebe2
Nikhil Marathe <nsm.nikhil@gmail.com>
2013-04-04 00:51:11 +0530

    Persona login and install support

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,6 @@
 function finishLogin(assertion) {
-  $.post('/login', { assertion: assertion }, function(data, status, xhr) {
-      console.log(arguments);
+  $.post('/weblogin', { assertion: assertion }, function(data, status, xhr) {
+      //window.location.reload();
   });
 }
 
@@ -16,11 +16,4 @@ $(function() {
   $('#persona-signin').click(function() {
     navigator.id.request();
   });
-
-  /*if (navigator.mozApps) {
-    $('#container').append('<a id="install-button" class="btn btn-primary offset4">Install</a>');
-    $('#install-button').click(function() {
-      navigator.mozApps.install("http://localhost:5000/static/manifest.webapp");
-    });
-  }*/
 });
--- server/app.py
+++ server/app.py
@@ -4,17 +4,23 @@ import json
 from redis import Redis
 
 app = Flask(__name__, static_url_path='/static', static_folder='../app', template_folder='../app')
+app.secret_key = 'topseekret'
 redis = Redis()
+
+def k(s):
+    return 'pushapptophone:%s'%s
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    endpointskey = k('%s:endpoints'%(session.get('email', '')))
+    return render_template('index.html', email=session.get('email', None), devices=redis.lrange(endpointskey, 0, 20))
 
 @app.route('/install', methods=['POST'])
 def install():
+    print request.form
     manifest = request.form['manifest']
-    redis.set('pushapptophone:manifest', manifest)
-    for endpoint in redis.lrange('pushapptophone:endpoints', 0, 25):
+    redis.set(k('manifest'), manifest)
+    for endpoint in redis.lrange(k('%s:endpoints'%session['email']), 0, 25):
         print endpoint
         r = requests.put(endpoint)
         print r
@@ -22,7 +28,34 @@ def install():
 
 @app.route('/manifest')
 def get_manifest():
-    return Response(redis.get('pushapptophone:manifest'), content_type='text/plain')
+    return Response(redis.get(k('manifest')), content_type='text/plain')
+
+@app.route('/weblogin', methods=['POST'])
+def weblogin():
+    if 'assertion' not in request.form:
+        abort(400)
+
+    # Send the assertion to Mozilla's verifier service.
+    data = {'assertion': request.form['assertion'], 'audience': 'http://pushtophone.nikhilism.com'}
+    resp = requests.post('https://verifier.login.persona.org/verify', data=data, verify=True)
+ 
+    # Did the verifier respond?
+    if resp.ok:
+        # Parse the response
+        verification_data = json.loads(resp.content)
+ 
+        # Check if the assertion was valid
+        if verification_data['status'] == 'okay':
+            # Log the user in by setting a secure session cookie
+            session.update({'email': verification_data['email']})
+            return verification_data['email']
+        else:
+            print 'status not okay', verification_data
+    else:
+        print 'resp.ok not true', resp
+ 
+    # Oops, something failed. Abort.
+    abort(500)
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -30,7 +63,7 @@ def login():
         abort(400)
 
     # Send the assertion to Mozilla's verifier service.
-    data = {'assertion': request.form['assertion'], 'audience': 'http://pushtophone.nikhilism.com'}
+    data = {'assertion': request.form['assertion'], 'audience': 'system.gaiamobile.org'}
     resp = requests.post('https://verifier.login.native-persona.org/verify', data=data, verify=True)
  
     # Did the verifier respond?
@@ -42,7 +75,7 @@ def login():
         if verification_data['status'] == 'okay':
             # Log the user in by setting a secure session cookie
             session.update({'email': verification_data['email']})
-            return resp.content
+            return verification_data['email']
         else:
             print 'status not okay', verification_data
     else:
@@ -54,11 +87,13 @@ def login():
 @app.route('/endpoint', methods=['POST'])
 def endpoint():
     endpoint = request.form['endpoint']
-    print endpoint
-    if not endpoint:
+    email = request.form['email']
+
+    print endpoint, email
+    if not endpoint or not email:
         abort(400)
 
-    redis.rpush('pushapptophone:endpoints', endpoint)
+    redis.rpush(k('%s:endpoints'%email), endpoint)
     return 'OK'
 
 if __name__ == '__main__':
